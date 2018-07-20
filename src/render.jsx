252ba30d064f7affba71d00b2109659289870101
lakhara <siddharth.lakhara@gmail.com>
2018-07-20 13:36:09 +0530

    boiler plate

---
+++ src/render.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+export default render;
