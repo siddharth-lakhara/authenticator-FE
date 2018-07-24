c7724d43c5f552624946c482d4f522b08ddb7ce4
lakhara <siddharth.lakhara@gmail.com>
2018-07-24 18:17:32 +0530

    added login page

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": "airbnb",
     "env": {
         "browser": true
+    },
+    "parser": "babel-eslint",
+    "rules": {
+        "react/jsx-one-expression-per-line": 0
     }
 };
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import './login.css';
 
 class Login extends React.Component {
+  state = {
+    userName: '',
+    password: '',
+  };
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onSubmit = () => {
+
+  }
+
   render() {
+    const { userName, password } = this.state;
+    const { onChange, onSubmit } = this;
     return (
-      <div>
-        Login Page
+      <div className="login">
+        <div className="login-border">
+          <div className="login-login">
+            LOGIN
+          </div>
+          <div className="login-inputItems">
+            <div className="login-userName">
+              <div className="login-text">User name:</div>
+              <input className="login-inputFields" name="userName" value={userName} type="text" onChange={onChange} />
+            </div>
+            <div className="login-password">
+              <div className="login-text">Password:</div>
+              <input className="login-inputFields" name="password" value={password} type="password" onChange={this.onChange} />
+            </div>
+            <button type="button" onClick={onSubmit} className="login-submit">Submit</button>
+          </div>
+        </div>
       </div>
     );
   }
