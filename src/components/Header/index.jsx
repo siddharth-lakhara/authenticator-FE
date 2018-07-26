3778ade6a843c4a141fbcb9d06e1da6f28966b15
lakhara <siddharth.lakhara@gmail.com>
2018-07-26 22:10:10 +0530

    logout button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
 
-const Header = ({ history }) => (
-  <div className="header">
-    <div>
-    Welcome !
-    </div>
-    <div className="ghost" />
+const logout = () => {
+  // const token = window.localStorage.getItem('token');
+  window.localStorage.removeItem('token');
+};
+
+const LoginButton = ({ history }) => (
+  <div>
     <button type="button" onClick={() => { history.push('/login'); }} className="loginButton">
       Log in
     </button>
@@ -15,5 +16,26 @@ const Header = ({ history }) => (
     </button>
   </div>
 );
+
+const LogoutButton = ({ history }) => (
+  <div>
+    <button type="button" onClick={() => { logout(); history.push('/login'); }} className="loginButton">
+      Log out
+    </button>
+  </div>
+);
+
+const Header = ({ history }) => {
+  const token = window.localStorage.getItem('token');
+  return (
+    <div className="header">
+      <div>
+        Welcome !
+      </div>
+      <div className="ghost" />
+      {token === null ? <LoginButton history={history} /> : <LogoutButton history={history} /> }
+    </div>
+  );
+};
 
 export default Header;
